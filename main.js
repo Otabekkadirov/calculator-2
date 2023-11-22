07433b2a53b23f766902a5203d35dda1163ad9c2
Otabekkadirov <ganijanovich94@gmail.com>
2023-11-22 16:01:06 +0500

    feat: add some js

--- main.js
+++ main.js
@@ -1,1 +1,31 @@
+// All elements from html
+const prevScreen = document.querySelector(".previous");
+const currentScreen = document.querySelector(".current");
 
+const allClearButton = document.querySelector(".all-clear");
+const deleteButton = document.querySelector(".delete");
+const equalButton = document.querySelector(".equal");
+
+const numberButtons = document.querySelectorAll(".number");
+const operationButtons = document.querySelectorAll(".operation");
+
+let currentOperand = "";
+
+numberButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+        updateScreen(button.textContent);
+        console.log(currentOperand);
+    });
+});
+
+function updateScreen(number) {
+    if (currentOperand === "0" && number === "0") return;
+
+    if (currentOperand === "0" && number !== "0" && number !== ".") {
+        currentOperand = number;
+    } else {
+        currentOperand += number;
+    }
+
+    currentScreen.textContent = currentOperand;
+}
